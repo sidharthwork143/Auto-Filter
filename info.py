9f2f44dfc8ffa320923ae6c5e0bd2050f4650af1
sidharthwork143 <sidharthwork143@gmail.com>
2025-06-23 18:12:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,7 +39,7 @@ BOT_ID = BOT_TOKEN.split(":")[0]
 PORT = int(environ.get('PORT', '80'))
 
 # Upload your images to "postimages.org" and get direct link
-PICS = (environ.get('PICS', 'https://i.postimg.cc/8C15CQ5y/1.png https://i.postimg.cc/gcNtrv0m/2.png https://i.postimg.cc/cHD71BBz/3.png https://i.postimg.cc/F1XYhY8q/4.png https://i.postimg.cc/1tNwGVxC/5.png https://i.postimg.cc/dtW30QpL/6.png https://i.postimg.cc/139dvs3c/7.png https://i.postimg.cc/QtXVtB8K/8.png https://i.postimg.cc/y8j8G1XV/9.png https://i.postimg.cc/zDF6KyJX/10.png https://i.postimg.cc/fyycVqzd/11.png https://i.postimg.cc/26ZBtBZr/13.png https://i.postimg.cc/PJn8nrWZ/14.png https://i.postimg.cc/cC7txyhz/15.png https://i.postimg.cc/kX9tjGXP/16.png https://i.postimg.cc/zXjH4NVb/17.png https://i.postimg.cc/sggGrLhn/18.png https://i.postimg.cc/y8pgYTh7/19.png')).split()
+PICS = (environ.get('PICS', 'https://files.catbox.moe/duf9oo.jpg https://files.catbox.moe/uq3nx0.jpg https://files.catbox.moe/jkyzwz.jpg https://files.catbox.moe/3i3qgn.jpg https://files.catbox.moe/ytv5jp.jpg https://files.catbox.moe/n9ypco.jpg')).split()
 
 # Bot Admins
 ADMINS = environ.get('ADMINS', '5555597971')
